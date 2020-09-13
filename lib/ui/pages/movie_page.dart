dab9a8e88820a1cc935851111a89e10bdc75f897
AdiatsaPutra <santikosetyo77@gmail.com>
2020-09-13 10:29:51 +0700

    adding movie_page and profile picture

---
+++ lib/ui/pages/movie_page.dart
@@ -0,0 +1,63 @@
+part of 'pages.dart';
+
+class MoviePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        // note: Header
+        Container(
+          decoration: BoxDecoration(
+            color: accentColor1,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20)),
+          ),
+          padding: EdgeInsets.fromLTRB(defaultMargin, 20, defaultMargin, 30),
+          child: BlocBuilder<UserBloc, UserState>(builder: (_, userState) {
+            if (userState is UserLoaded) {
+              return Row(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Color(0xFF5F558B), width: 1),
+                    ),
+                    child: Stack(
+                      children: [
+                        SpinKitFadingCircle(
+                          color: accentColor2,
+                          size: 50,
+                        ),
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                  image: (userState.user.profilePicture == ""
+                                      ? Icon(Icons.person)
+                                      : NetworkImage(
+                                          userState.user.profilePicture)))),
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [Text("data"), Text("data")],
+                  )
+                ],
+              );
+            } else {
+              return SpinKitFadingCircle(
+                color: accentColor2,
+                size: 50,
+              );
+            }
+          }),
+        )
+      ],
+    );
+  }
+}
